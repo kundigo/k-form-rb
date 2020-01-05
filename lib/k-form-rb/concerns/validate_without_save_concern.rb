057b108efcbac3bb510ddcc5e6dc63752d0e1f78
Dorian LUPU <dorian@kundigo.pro>
2020-01-05 22:43:09 +0100

    add-custom-types

--- lib/k-form-rb/concerns/validate_without_save_concern.rb
+++ lib/k-form-rb/concerns/validate_without_save_concern.rb
@@ -4,15 +4,15 @@ module KFormRb
       extend ActiveSupport::Concern
 
       included do
-        attr_accessor :_force_rollback
-        attribute :_force_rollback, :bool
-        after_save :force_rollback!, if: :_force_rollback
+        attr_accessor :_prevent_save
+        attribute :_prevent_save, :bool
+        after_save :force_rollback!, if: :_prevent_save
       end
 
       private
 
       def force_rollback!
-        raise ActiveRecord::Rollback.new("_force_rollback")
+        raise ActiveRecord::Rollback.new("_prevent_save")
       end
     end
   end
