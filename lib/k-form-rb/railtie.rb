c429a186eb51c7afd4be129a8406472160acb8d7
Dorian LUPU <dorian@kundigo.pro>
2020-01-05 15:08:12 +0100

    fix rake file addition

--- lib/k-form-rb/railtie.rb
+++ lib/k-form-rb/railtie.rb
@@ -18,7 +18,7 @@ module KFormRb
     end
 
     rake_tasks do
-      path = File.expand_path(__dir__)
+      path = File.expand_path('..', __dir__)
       Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
     end
   end
