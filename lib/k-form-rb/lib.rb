6ace5fc02a8275c1a929a40f57b9c92b2f5dcf43
Kundigo CI <dorian+ci@kundigo.pro>
2023-01-26 22:24:48 +0000

    new commit generated from f9d61abe55328da866039bf47d62d79c89f0dea6

--- lib/k-form-rb/lib.rb
+++ lib/k-form-rb/lib.rb
@@ -2,6 +2,7 @@ require_relative 'lib/bool'
 require_relative 'lib/full_access_ability'
 require_relative 'lib/instrumentation_utils'
 require_relative 'lib/visual_testing'
+
 module KFormRb
   module Lib
   end
