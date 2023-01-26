8c5babd7873b5f90c8fe6b9baffb23b144751149
Kundigo CI <dorian+ci@kundigo.pro>
2023-01-26 18:53:17 +0000

    new commit generated from b733c8f716f725bddaf0630802267030ff9b5a75

--- lib/k-form-rb/lib.rb
+++ lib/k-form-rb/lib.rb
@@ -2,6 +2,8 @@ require_relative 'lib/bool'
 require_relative 'lib/full_access_ability'
 require_relative 'lib/instrumentation_utils'
 require_relative 'lib/visual_testing'
+require_relative 'lib/site_prism_components'
+
 module KFormRb
   module Lib
   end
---
+++ lib/k-form-rb/lib/site_prism_components.rb
@@ -0,0 +1,16 @@
+require_relative 'site_prism_components/autocomplete_component'
+require_relative 'site_prism_components/checkbox_component'
+require_relative 'site_prism_components/date_component'
+require_relative 'site_prism_components/select_component'
+require_relative 'site_prism_components/text_area_component'
+require_relative 'site_prism_components/text_input_component'
+require_relative 'site_prism_components/toggle_switch_component'
+require_relative 'site_prism_components/quill_editor_component'
+require_relative 'site_prism_components/datetime_component'
+
+module KFormRb
+  module Lib
+    module SitePrismComponents
+    end
+  end
+end
